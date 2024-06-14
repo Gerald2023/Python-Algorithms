f8c87dd4e519b4701a3b7b3f219b09bb97509002
Gerald2023 <123606370+Gerald2023@users.noreply.github.com>
2024-06-14 10:31:23 -0500

    added more basics

---
+++ IntroToPython/Basics/math.py
@@ -0,0 +1,34 @@
+
+# Division is decimal by default
+print(5 / 2)
+
+# double slash rounds down
+print(5 // 2)
+
+# CAREFUL: most languages round towards 0 by
+# default so negative numbers will round down
+
+print(-3 // 2)
+
+# A workaround for rounding towards zero is to
+# use decimal division and then convert to int.
+print(int(-3 / 2))
+
+# Modding is similar to most languages
+print(10 % 3)
+
+# Except for negative values
+print(-10 % 3)
+
+# To be consistent with other languages modulo
+import math
+print(math.fmod(-10, 3))
+
+# More math helpers
+print("More math helpers")
+print(math.floor(3 / 2))
+print(math.ceil(3 / 2))
+print (math.sqrt (4))
+print (math.pow(2,3))
+
+# Max / Mint Int
